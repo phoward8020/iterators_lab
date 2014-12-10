e6ad2186776d5ca440422b3cb1c0af2d8a18f478
Paul Howard <paul@paul-howard.net>
2014-12-09 16:24:50 -0800

    nothing, really

--- reddit.js
+++ reddit.js
@@ -3,31 +3,39 @@ var reddit = require("./data.json")
 // Write your solutions below
 
 // 1. "forEach" - console.log all titles
+
 var printTitles = function (element, index) {
     console.log('>> ' + element.data.title);
 };
+
 console.log('TITLES');
 console.log('-----------------------------------------------------');
+
 reddit["data"].children.forEach(printTitles);
+
 console.log(reddit["data"].children.length);
 console.log('-----------------------------------------------------\n\n');
 
 
 // 2. "map" - Return an array of permalinks, that contain full http:// path to reddit
+
 var getPermalink = function (element, index) {
     return "http://reddit.com/" + element.data.permalink;
 }
 var permalinks = reddit["data"].children.map(getPermalink);
-console.log('PERMALINKS');
+
+console.log('PERMALINKS');a
 console.log('-----------------------------------------------------');
 console.log(permalinks);
 console.log('-----------------------------------------------------\n\n');
 
 // 3. "filter" - Filter the posts that contain the selftext key
+
 var getPostsWithSelftext = function (element, index) {
     return element.data.selftext !== "";
 };
 var postsWithSelftext = reddit["data"].children.filter(getPostsWithSelftext);
+
 console.log('POSTS WITH SELFTEXT');
 console.log('-----------------------------------------------------');
 console.log(postsWithSelftext);
--- reddit2.js
+++ reddit2.js
@@ -4,13 +4,23 @@ var reddit = require("./data.json")
 
 // 1. "forEach" - console.log all titles
 var printTitles = function (element, index) {
-    console.log(element.data.title);
+    console.log('TITLES');
+    console.log('-----------------------------------------------------');
+    console.log(">>" + element.data.title + "\n");
 };
 
 reddit["data"].children.forEach(printTitles);
 
 
 // 2. "map" - Return an array of permalinks, that contain full http:// path to reddit
+var printPermalinks = function (element, index) {
+    var permalinks = 
+    console.log('PERMALINK');
+    console.log('-----------------------------------------------------');
+    console.log();
+}
+reddit["data"].children.map(printPermalinks)
+
 
 // 3. "filter" - Filter the posts that contain the selftext key
 
